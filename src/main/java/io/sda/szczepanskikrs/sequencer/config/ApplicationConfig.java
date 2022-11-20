f85e3f01cef07ab69965dc35cc2eb63845bd82c7
Krzysztof Szczepański <kr.szczepanski@protonmail.com>
2022-11-20 15:54:49 +0100

    Add docs

---
+++ src/main/java/io/sda/szczepanskikrs/sequencer/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package io.sda.szczepanskikrs.sequencer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+// Springu weź moje metody i zrób z nich beany
+@Configuration
+@Slf4j
+public class ApplicationConfig {
+
+    @Bean
+    // To będzie nasz komponent
+    Consumer<String> output(){return log::info;}
+}
--- src/main/java/io/sda/szczepanskikrs/sequencer/infrastructure/CommandLineSequencerService.java
+++ src/main/java/io/sda/szczepanskikrs/sequencer/infrastructure/CommandLineSequencerService.java
@@ -2,6 +2,7 @@ package io.sda.szczepanskikrs.sequencer.infrastructure;
 
 import io.sda.szczepanskikrs.sequencer.api.BaseCountConverter;
 import io.sda.szczepanskikrs.sequencer.api.dto.DnaBaseCountDto;
+import io.sda.szczepanskikrs.sequencer.api.dto.ThymineDto;
 import io.sda.szczepanskikrs.sequencer.dna.DnaBaseCount;
 import io.sda.szczepanskikrs.sequencer.dna.DnaBaseCounter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +11,26 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.time.Instant;
+import java.util.function.Consumer;
 
 @Service
 public class CommandLineSequencerService implements CommandLineRunner {
     // Finalne pole wstrzykiwane przez konstruktor.
     private final DnaBaseCounter baseCounter;
     private final BaseCountConverter<DnaBaseCountDto> converter;
+    private final BaseCountConverter<ThymineDto> thymineDtoBaseCountConverter;
+    private final Consumer<String> outputConsumer;
 
     //Autowired nie jest wymagane ale jest dobrą praktyką przy wstrzykiwaniu przez konstruktor.
     @Autowired
     public CommandLineSequencerService(
             DnaBaseCounter baseCounter,
-            BaseCountConverter<DnaBaseCountDto> converter
-    ) {
+            BaseCountConverter<DnaBaseCountDto> converter,
+            BaseCountConverter<ThymineDto> thymineDtoBaseCountConverter, Consumer<String> outputConsumer) {
         this.baseCounter = baseCounter;
         this.converter = converter;
+        this.thymineDtoBaseCountConverter = thymineDtoBaseCountConverter;
+        this.outputConsumer = outputConsumer;
     }
 
     @Override
@@ -34,9 +40,12 @@ public class CommandLineSequencerService implements CommandLineRunner {
         // Konwersja do DTO
         DnaBaseCount dnaBaseCount = baseCounter.calculateDnaBaseCount(sequence);
         DnaBaseCountDto dto = converter.convertToType(dnaBaseCount);
-
+        // Konwersja do Thyminy
+        ThymineDto thymineDto = thymineDtoBaseCountConverter.convertToType(dnaBaseCount);
         // Printujemy DnaBaseCountDto
-        System.out.println(dto);
+        outputConsumer.accept(dto.toString());
+        // Printujemy ThymineDTO
+        outputConsumer.accept(thymineDto.toString());
     }
 
     @PostConstruct
@@ -45,3 +54,27 @@ public class CommandLineSequencerService implements CommandLineRunner {
         System.out.println(Instant.now());
     }
 }
+
+/**
+ * @SpringBootApplication - Startuje aplikacje (od classpatha przez skanowanie komponentów)
+ * @SpringBootTest - Przygotowuje kontekst testowy i startuje aplikacje gotową do testów.
+ * @Component - Oznacza co ma być wzięte pod uwagę przy starcie aplikacji (na poziomie klasy).
+ * @Bean -  Oznacza co ma być wzięte pod uwagę przy starcie aplikacji (na poziomie metody).
+ * @Service = @Component
+ * @Respository = @Component
+ * @Configuration = Oznacza że dana klasa zawiera konfigurajcę w postaci @Beanów
+ * @TestConfiguration = @Configuration tylko z Beanami które są testowe.
+ * @Autowired = pola, metody i konstruktory które mają być wstrzyknięte
+ * @Primary = w przypadku kiedy mamy wiele komponentów wybieraj ten jako domyślny
+ * @Scope = definiuje ile instancji będzie tworzonych i jaki będzie ich cykl życia ( domyślnie Singleton ).
+ * - singleton
+ * - prototype
+ * - request
+ * - session
+ * - globalSession
+ * - application
+
+ * @PreDestory - Może(ale nie musi) być wywołany kiedy bean jest niszczony przez kontekst.
+ * @PostConstruct - Wywoływane po stworzeniu beana, do wszelkiego rodzaju setupów.
+ * @ComponentScan - Pozwala na zdefiniowanie pakietów które mają być skanowane, domyślnie skanuje pakiet klasy na którą go nakładamy.
+ */
